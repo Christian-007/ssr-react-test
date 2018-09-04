498d1167c26f3542772f9953246bdc0feb3a10ad
ilmuonedata-chris <christian@ilmuonedata.com>
2018-09-04 09:32:57 +0700

    Initial commit

---
+++ server/bootstrap.js
@@ -0,0 +1,16 @@
+require('ignore-styles');
+
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+
+require('babel-register')({
+  ignore: [ /(node_modules)/ ],
+  presets: ['es2015', 'react-app'],
+  plugins: [
+    'syntax-dynamic-import',
+    'dynamic-import-node',
+    'react-loadable/babel'
+  ]
+});
+
+require('./index');
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Loadable from 'react-loadable';
+
+const AsyncComponent = Loadable({
+  loader: () => import("./SomeComponent"),
+  loading: () => <div>loading...</div>,
+});
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <AsyncComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
